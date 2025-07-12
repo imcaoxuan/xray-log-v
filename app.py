62560a763130ce3ab3e937236372dae0f54b7874
caoxuan <caoxuan@imcaoxuan.cn>
2025-07-12 20:00:28 +0800

    update

--- app.py
+++ app.py
@@ -117,16 +117,16 @@ def dump2mysql():
                         )
                         access_list.append(access)
                     if len(access_list) == BATCH_SIZE:
+                        print(f'add {len(access_list)} records to mysql')
                         db.session.add_all(access_list)
                         db.session.commit()
-                        print(f'add {len(access_list)} records to mysql')
                         access_list.clear()
                 except Exception as e:
                     print(e)
                 finally:
+                    print(f'add {len(access_list)} records to mysql')
                     db.session.add_all(access_list)
                     db.session.commit()
-                    print(f'add {len(access_list)} records to mysql')
                     access_list.clear()
                     db.session.close_all()
         print(f"dump2mysql ended at: {time.strftime('%Y-%m-%d %H:%M:%S')}")
